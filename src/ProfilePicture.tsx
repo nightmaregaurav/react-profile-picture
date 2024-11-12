f390ff20466de8320cf5d1c9f4863054678f3ac9
Gaurav Nyaupane <nightmaregaurav@users.noreply.github.com>
2024-11-12 23:00:26 +0545

    Fixed issue with offline profile's border radius

--- src/ProfilePicture.tsx
+++ src/ProfilePicture.tsx
@@ -1,6 +1,8 @@
 import React, {CSSProperties} from 'react';
 import "./profilePicture.css";
 
+const BORDER_WIDTH = "3px";
+
 interface ProfilePictureProps {
   size: Exclude<CSSProperties["width"], undefined>;
   imageUrl: string;
@@ -23,11 +25,13 @@ const ProfilePicture = (
       className={`profile-picture`}
       style={{
         backgroundImage: `url(${imageUrl})`,
-        width: `calc(${size} - 3px)`,
-        height: `calc(${size} - 3px)`,
+        width: `calc(${size} - ${BORDER_WIDTH} - ${BORDER_WIDTH})`,
+        height: `calc(${size} - ${BORDER_WIDTH} - ${BORDER_WIDTH})`,
+        borderWidth: (showOnlineBadge && isOnline) ? BORDER_WIDTH : "0px",
         borderColor: (showOnlineBadge && isOnline)
           ? onlineBadgeColor
           : "rgba(0, 0, 0, 0)",
+        padding: (showOnlineBadge && isOnline) ? "0px" : BORDER_WIDTH,
       }}
     />
   );
